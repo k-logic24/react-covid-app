21d2a22ccf5497ab8e11a3de9b7527c19bf6f488
岩ちゃん <iwater0715@gmail.com>
2020-08-23 17:40:18 +0900

    material-practice

--- src/components/MaterialUI.tsx
+++ src/components/MaterialUI.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import {Button, makeStyles} from '@material-ui/core'
+import {Button, makeStyles, Typography, Grid, Paper} from '@material-ui/core'
 
 const useStyles = makeStyles({
   btnStyle: {
     background: `green`,
     padding: `3px 50px`,
+  },
+  typeStyle: {
+    color: `blue`
+  },
+  paperStyle: {
+    background: `orange`,
+    height: `50px`
   }
 })
 
@@ -13,6 +20,23 @@ const MaterialUI: React.FC = () => {
 
   return (
     <div>
+      <Typography
+        className={classes.typeStyle}
+        color="secondary"
+        variant="h1"
+        align="left"
+        gutterBottom
+      >
+        Hello UI
+      </Typography>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <Paper className={classes.paperStyle}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paperStyle}>xs=12</Paper>
+        </Grid>
+      </Grid>
       <Button className={classes.btnStyle} variant="contained" color="primary">
         TEST BUTTON
       </Button>

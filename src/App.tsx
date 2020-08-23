2df3c0f680ac92fb70dcd740aac3f4577e45e68c
岩ちゃん <iwater0715@gmail.com>
2020-08-23 15:23:40 +0900

    leading-in-materialui

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import './App.css'
 import LinePlot from "./components/LinePlot"
 import PiePlot from "./components/PiePlot"
 import BarPlot from "./components/BarPlot"
-import RadarPlot from "./components/RadarPlot";
-import BubblePlot from "./components/BubblePlot";
+import RadarPlot from "./components/RadarPlot"
+import BubblePlot from "./components/BubblePlot"
+import MaterialUI from "./components/MaterialUI"
 
 const App = () => {
   return (
     <div className="App">
-      <LinePlot />
-      <PiePlot />
-      <BarPlot />
-      <RadarPlot />
-      <BubblePlot />
+      {/*<LinePlot />*/}
+      {/*<PiePlot />*/}
+      {/*<BarPlot />*/}
+      {/*<RadarPlot />*/}
+      {/*<BubblePlot />*/}
+      <MaterialUI />
     </div>
   )
 }
---
+++ src/components/MaterialUI.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from '@material-ui/core'
+
+const MaterialUI: React.FC = () => {
+  return (
+    <div>
+      <Button variant="contained" color="secondary">TEST BUTTON</Button>
+    </div>
+  )
+}
+
+export default MaterialUI

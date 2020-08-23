4d84a53b05187ba21d17755621a2dae90be1f8c7
岩ちゃん <iwater0715@gmail.com>
2020-08-23 15:15:46 +0900

    chart-demo-complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import './App.css'
 
 import LinePlot from "./components/LinePlot"
 import PiePlot from "./components/PiePlot"
+import BarPlot from "./components/BarPlot"
+import RadarPlot from "./components/RadarPlot";
+import BubblePlot from "./components/BubblePlot";
 
 const App = () => {
   return (
     <div className="App">
       <LinePlot />
       <PiePlot />
+      <BarPlot />
+      <RadarPlot />
+      <BubblePlot />
     </div>
   )
 }

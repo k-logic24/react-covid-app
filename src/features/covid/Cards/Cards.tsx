70918bc702504d77fc02ed5913b9e6a0eb416636
岩ちゃん <iwater0715@gmail.com>
2020-08-24 13:03:26 +0900

    create-redux

---
+++ src/features/covid/Cards/Cards.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {useSelector} from "react-redux"
+import {selectData} from "../covidSlice"
+import CountUp from "react-countup"
+import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import {GiHastyGrave} from 'react-icons/gi'
+import {MdLocalHospital} from 'react-icons/md'
+import {AiFillLike} from 'react-icons/ai'
+ 
+import styles from './Cards.module.css'
+
+const Cards = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cards

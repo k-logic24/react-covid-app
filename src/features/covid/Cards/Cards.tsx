80f176236081bbc8078e1de9da8aea754b4a8ac8
岩ちゃん <iwater0715@gmail.com>
2020-08-24 21:48:27 +0900

    create-cards-component

--- src/features/covid/Cards/Cards.tsx
+++ src/features/covid/Cards/Cards.tsx
@@ -9,10 +9,61 @@ import {AiFillLike} from 'react-icons/ai'
  
 import styles from './Cards.module.css'
 
-const Cards = () => {
+const Cards: React.FC = () => {
+  const data = useSelector(selectData)
+
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <Grid container spacing={1} justify="center">
+        <Grid item xs={12} md={3} component={Card} className={styles.infected}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <MdLocalHospital />
+              Infected persons
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={data.confirmed.value}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={styles.recovered}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <AiFillLike />
+              Recovered persons
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={data.recovered.value}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={styles.deaths}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <GiHastyGrave />
+              Deaths persons
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={data.deaths.value}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </div>
   )
 }

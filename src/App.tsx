076858718b3876fa54ab77c25cfeedf6aee0b504
岩ちゃん <iwater0715@gmail.com>
2020-08-25 12:39:23 +0900

    一応dashboard完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react'
 
-import Cards from "./features/covid/Cards/Cards"
-import Chart from "./features/covid/Chart/Chart"
-import PieChart from "./features/covid/PieChart/PieChart"
-import SwitchCountry from "./features/covid/SwitchCountry/SwitchCountry"
+
 import './App.css'
+import DashBoard from "./features/covid/DashBoard/DashBoard"
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Cards />
-        <Chart />
-        <PieChart />
-        <SwitchCountry />
-      </header>
-    </div>
+    <DashBoard />
   )
 }
 
---
+++ src/features/covid/DashBoard/DashBoard.tsx
@@ -0,0 +1,73 @@
+import React, {useEffect} from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import {useSelector, useDispatch} from 'react-redux'
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Grid
+} from '@material-ui/core'
+import {fetchAsyncGet, fetchAsyncGetDaily, selectData} from '../covidSlice'
+import styles from './DashBoard.module.css'
+import App from '../../../App'
+import SwitchCountry from '../SwitchCountry/SwitchCountry'
+import Cards from '../Cards/Cards'
+import Chart from '../Chart/Chart'
+import PieChart from '../PieChart/PieChart'
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1
+  },
+  content: {
+    marginTop: 85
+  }
+}))
+
+const DashBoard: React.FC = () => {
+  const classes = useStyles()
+  const dispatch = useDispatch()
+  const data = useSelector(selectData)
+
+  useEffect(() => {
+    dispatch(fetchAsyncGet())
+    dispatch(fetchAsyncGetDaily())
+  }, [dispatch])
+
+  return (
+    <div>
+      <AppBar position="absolute">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Covid 19 Live Dashboard
+          </Typography>
+          {data && (
+            <Typography variant="body1">
+              {new Date(data.lastUpdate).toDateString()}
+            </Typography>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.content}>
+        <div className={styles.container}>
+          <SwitchCountry />
+        </div>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={7}>
+            <Chart />
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <PieChart />
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <Cards />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  )
+}
+
+export default DashBoard
